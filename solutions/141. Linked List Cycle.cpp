f0784dbaedded9f5b91ba2615ceac13ccee5cbdf
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-01-11 10:17:58 +0530

    8 ms

---
+++ solutions/141. Linked List Cycle.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head) return false;
+        ListNode *fast = head->next;
+        ListNode *slow = head;
+        
+        while(fast != NULL && fast->next != NULL && slow != NULL){
+            
+            if(fast == slow){
+                return true;
+            }
+            fast = fast->next->next;
+            slow = slow->next;
+            
+        }
+        return false;
+    }
+};
