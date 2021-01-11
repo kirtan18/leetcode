648579aafefcdc9adc07ef7b0e99c5361be0a66b
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-01-11 10:06:27 +0530

    124 ms

---
+++ solutions/697. Degree of an Array.cpp
@@ -0,0 +1,34 @@
+​
+​
+​
+class Solution {
+public:
+    int findShortestSubArray(vector<int>& nums) {
+        
+    map<int, int> freq;
+    map<int, vector<int>> pos;
+    int mx = INT_MIN;
+    
+    /*
+    get frequency of each number in array
+    get highest degree
+    note the positions of frequencies
+    */    
+    for(int i = 0; i < nums.size(); i++)
+    {
+        mx = max(mx, ++freq[nums[i]]);        
+        pos[nums[i]].push_back(i);
+    }
+       
+    //get shortest distance
+    int dist = INT_MAX;
+    for(auto num : nums)
+    {
+        if(freq[num] == mx)            
+            dist = min(dist, pos[num].back() - pos[num].front());         
+    }
+    
+    return dist + 1;
+        
+    }
+};
