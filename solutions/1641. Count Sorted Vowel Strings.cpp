025274753672cc9a7cbc08cf7139e2a9f7f5af67
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-01-17 21:45:11 +0530

    4 ms

--- solutions/1641. Count Sorted Vowel Strings.cpp
+++ solutions/1641. Count Sorted Vowel Strings.cpp
@@ -1,5 +1,15 @@
 class Solution {
 public:
+    // math solution
+    // space = O(1)
+        int countVowelStrings(int n) {
+              return (n + 1) * (n + 2) * (n + 3) * (n + 4) / 24;
+        }
+};
+​
+ 
+    /*
+    // time  = O(n) space = O(1)
     int countVowelStrings(int n) {
         int a = 1;
         int e = 1;
@@ -12,8 +22,8 @@ public:
             i += e;
             o += i;
             u += o;
-        }
+        }
        
        return a+e+i+o+u; 
-    }
-};
+    }
+};*/
