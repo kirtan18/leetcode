d115cdcf089718d288c599f81a01b8048e0fbaae
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-01-19 22:56:38 +0530

    148 ms

--- solutions/5. Longest Palindromic Substring.cpp
+++ solutions/5. Longest Palindromic Substring.cpp
@@ -1,49 +1,64 @@
-class Solution {
-public:
-    // time = O(n2)  space == O(1)
-    string longestPalindrome(string s) {
-        if(s.length() <= 1)return s;
-        int max_len = 1;
-        int n = s.length();
-        int st=0,end = 0;
-        
-        // odd length
-        for(int i=0;i<n-1;++i){
-            int l = i, r= i;
-            while(l >= 0 && r < n){
-                if(s[l] == s[r]){
-                    l--;
-                    r++;
-                }else
-                    break;
-            }
-            int len  = r-l-1;
+            int len  = r-l-1;
             if(len > max_len){
                 max_len = len;
                 st = l+1;
                 end = r-1;
                 
-            }
-        }
-        
-        //even
-           for(int i=0;i<n-1;++i){
-            int l = i, r= i+1;
-            while(l >= 0 && r < n){
-                if(s[l] == s[r]){
-                    l--;
-                    r++;
-                }else
-                    break;
-            }
-            int len  = r-l-1;
-            if(len > max_len){
-                max_len = len;
-                st = l+1;
-                end = r-1;
-                
-            }
-        }
+            }
+        }
         return s.substr(st,max_len);
-    }
+    }
 };
+*/
+​
+    
+    /*************   using DP   ***********/
+      
+// time = O(n2)  space == O(n2)
+    
+ string longestPalindrome(string s) {
+     int n = s.size();
+     
+     if(n==0)return "";
+     
+     bool dp[n][n];
+     
+     memset(dp,0,sizeof(dp));
+     
+     for(int i = 0; i< n;i++){
+         dp[i][i] = true;
+     }
+     
+     string ans ="";
+     ans += s[0];
+     
+     for(int i=n-1;i>=0;i--){
+         for(int j=i+1;j<n;j++){
+             
+             if(s[i] == s[j]){
+                 
+                 if(j-i == 1 || dp[i+1][j-1]){
+                     dp[i][j] = true;
+                     
+                     if(ans.size() < j-i+1){
+                      ans =  s.substr(i,j-i+1);
+                     }
+                 }
+             }
+         }
+     }
+       
+     return ans;
+     
+  }
+};
+​
+​
+​
+​
+​
+​
+​
+​
+​
+​
