8ad2bd04b025d1a826b98e1b057c95be519a5416
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-01-20 00:27:22 +0530

    4 ms

--- solutions/151. Reverse Words in a String.cpp
+++ solutions/151. Reverse Words in a String.cpp
@@ -1,5 +1,35 @@
 class Solution {
 public:
+    
+    string reverseWords(string s) {
+        int n = s.size();
+        
+        stack<string> st;
+        string ans;
+        
+        for(int i = 0 ; i < n ; i ++){
+            string word;
+            
+            if(s[i] == ' ') continue;
+            
+            while(i<n && s[i] != ' ' ){
+                word += s[i];
+                i++;
+            }
+            st.push(word);
+        }
+        
+        while(!st.empty()){
+            ans += st.top();
+            st.pop();
+            if(!st.empty())   ans += " ";
+        }
+        
+        return ans;
+    }
+    
+    /*
+    
     // time == O(n)  space = O(n)
     string reverseWords(string s) {
        stringstream ss(s);
@@ -12,10 +42,11 @@ public:
         string ans = "";
         for(int i = v.size()-1 ; i>=0 ;i--){
             ans += v[i] + ' ';
-        }
+        }
         ans.erase(ans.end()-1);
         return ans;
-    }
+    }
+    */
 };
 ​
 ​
