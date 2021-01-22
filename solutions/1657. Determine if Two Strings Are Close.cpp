6cd9693ecede1555830c7712e9742adb134e3c6f
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-01-22 23:23:07 +0530

    72 ms

--- solutions/1657. Determine if Two Strings Are Close.cpp
+++ solutions/1657. Determine if Two Strings Are Close.cpp
@@ -20,5 +20,30 @@ bool closeStrings(string word1, string word2) {
     sort(counts2.begin(),counts2.end());
 ​
     return counts1 == counts2;
-}
+  }
 };
+/*
+​
+// time = O(n)  space == O(1)
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        vector<int> v1(26,0) , v2(26,0) , w1(26,0) , w2(26,0);
+        
+        for(char c : word1 ){
+            v1[c - 'a'] ++;
+            w1[c - 'a'] = 1;
+        }
+        
+        for(char c : word2 ){
+            v2[c - 'a'] ++;
+            w2[c - 'a'] = 1;
+        }
+        sort(v1.begin() , v1.end());
+        sort(v2.begin() , v2.end());
+        
+        return v1 == v2 && w1 == w2; 
+    }
+};
+​
+*/
