3e2bc058fc9c2a6eb06685994afb4aabf8f6932d
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-01-29 10:59:06 +0530

    44 ms

--- solutions/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.cpp
+++ solutions/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.cpp
@@ -1,5 +1,26 @@
 class Solution {
 public:
+    
+ /*   first  remember all ascii val     
+'0' => 48 
+'1' => 49 
+'2' => 50 
+'3' => 51 
+'4' => 52 
+'5' => 53 
+'6' => 54 
+'7' => 55 
+'8' => 56 
+'9' => 57 
+    
+    using asciii  val  in this  here  means  
+        1.   ans = 51(3) - 48(0)  = 3  here   max(3,0) = 3     0 rep = 48(ascii val)
+        2.   ans = 52(2) - 48(0)  = 2  here   max(2,3) = 3
+         
+        
+        so ans  is   3   in first case ans  same  here  to all cases  try  it  your self to dry run 
+          */
+     
     int minPartitions(string n) {
          int answer=0;
         
@@ -10,9 +31,9 @@ public:
 };
 ​
 /*
+ second approch
 ​
-​
-t temp=0;
+        temp=0;
         int mx=0;
       for(int i=0;i<s.size();i++)
       {
