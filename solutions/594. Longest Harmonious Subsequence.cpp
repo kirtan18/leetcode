8f20bca316eefdb56d3f0633283f49f052f27ac1
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-02-04 18:48:04 +0530

    76 ms

--- solutions/594. Longest Harmonious Subsequence.cpp
+++ solutions/594. Longest Harmonious Subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    
+    // [1,3,2,2,5,2,3,7]
     // time == O(n)  space == O(n)
     
     int findLHS(vector<int>& nums) {
@@ -22,3 +22,28 @@ public:
         return maxl;
     }
 };
+​
+   // in java but brute force   time = O(n2)  space = O(1)
+​
+/*
+public class Solution {
+    public int findLHS(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            boolean flag = false;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] == nums[i])
+                    count++;
+                else if (nums[j] + 1 == nums[i]) {
+                    count++;
+                    flag = true;
+                }
+            }
+            if (flag)
+                res = Math.max(count, res);
+        }
+        return res;
+    }
+}
+*/
