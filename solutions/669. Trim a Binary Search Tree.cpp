be225bc247a406fa7e032b1b9aeb8874cb7ce5cb
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-03-14 21:41:03 +0530

    16 ms

--- solutions/669. Trim a Binary Search Tree.cpp
+++ solutions/669. Trim a Binary Search Tree.cpp
@@ -9,22 +9,23 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+​
+          /*        Time Comp = O(n)  ,  space = O(h)    */ 
 class Solution {
 public:
     TreeNode* trimBST(TreeNode* root, int low, int high) {
+        if(root == NULL){
+            return NULL;
+        }
+        if(root->val < low){
+            return trimBST(root->right , low , high);
+        }
+        if(root->val > high){
+            return trimBST(root->left,low,high);
+        }
+        root->left  = trimBST(root->left,low,high);
+        root->right = trimBST(root->right,low,high);
         
-        if(root == NULL)return root;
-        
-        if(root->val >= low && root->val <= high){
-            root->left = trimBST(root->left,low,high);
-            root->right = trimBST(root->right,low,high);
-        }
-        else if(root->val < low){
-            root =  trimBST(root->right,low,high);
-        }
-        else if(root->val > high){
-            root = trimBST(root->left,low,high);
-        }
-       return root;   
-    }
+        return root;
+    }
 };
