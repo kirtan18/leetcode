a9bb91b9c0557f37be394b9e2d098c57bb09d5ad
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-03-17 02:21:46 +0530

    112 ms

---
+++ solutions/121. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,23 @@
+​
+/*           Time == O(N)     space == O(1)    */
+​
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;
+        int maxPrice = 0;
+        
+        for(int i=0;i<prices.size();i++){
+            if(prices[i] < minPrice){
+                minPrice = prices[i];
+            }
+            if(prices[i] - minPrice > maxPrice){
+                maxPrice = prices[i] - minPrice;
+            }
+        }
+        return maxPrice;
+    }
+    
+}; 
+    
+​
