e8e369ea9117aa18d6b0b54d45dd5af9bfcaa357
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-04-02 15:05:06 +0530

    4 ms

---
+++ solutions/1071. Greatest Common Divisor of Strings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    
+    int findgcd(int a , int b){
+        if(b == 0)return a;
+        return findgcd(b,a%b);
+    }
+    
+    
+    string gcdOfStrings(string str1, string str2) {
+        
+        if( str1 + str2 != str2 + str1)return "";
+        
+        int gcd = findgcd(str1.length() , str2.length());
+        
+        return str1.substr(0,gcd);
+        
+    }
+};
