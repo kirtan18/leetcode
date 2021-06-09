a771a33dcc96506e96d683acb69a080ef2fcb7f2
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-06-09 11:14:36 +0530

    0 ms

--- solutions/103. Binary Tree Zigzag Level Order Traversal.cpp
+++ solutions/103. Binary Tree Zigzag Level Order Traversal.cpp
@@ -1,7 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
@@ -9,14 +5,11 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// time = O(n)  space = O(n)
-#include<bits/stdc++.h>
 class Solution {
 public:
-     vector<vector<int>>ans;
+    vector<vector<int>>ret;      
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        if(!root)return  {};
-        
+        if(!root)return {};
         stack<TreeNode*>s1;
         stack<TreeNode*>s2;
         vector<int>temp;
@@ -25,11 +18,31 @@ public:
         
         while(!s1.empty() || !s2.empty()){
             
-           if(k%2 == 0){ 
-            while(!s1.empty()){
-              TreeNode*curr = s1.top(); s1.pop();
-                temp.push_back(curr->val);
-                if(curr->left) s2.push(curr->left);
-                if(curr->right) s2.push(curr->right);
-              }
-               k++;
+            if(k%2 == 0){
+                while(!s1.empty()){
+                    TreeNode * curr = s1.top();
+                    s1.pop();
+                    temp.push_back(curr->val);                    
+                    if(curr->left)s2.push(curr->left);
+                    if(curr->right)s2.push(curr->right);
+                    
+                }
+                k++;
+            }
+            else if(k%2 != 0){
+                while(!s2.empty()){
+                    TreeNode * curr = s2.top();
+                    s2.pop();
+                    temp.push_back(curr->val);
+                    
+                    if(curr->right)s1.push(curr->right);
+                    if(curr->left)s1.push(curr->left);
+                }
+                k++;
+            }
+            ret.push_back(temp);
+            temp.clear();   
+        }
+        return ret;
+    }
+};
