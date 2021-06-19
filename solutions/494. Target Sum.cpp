e8f7a836baede00f077e7e377959211b1327fb1c
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-06-20 00:02:11 +0530

    8 ms

--- solutions/494. Target Sum.cpp
+++ solutions/494. Target Sum.cpp
@@ -1,40 +1,14 @@
 class Solution {
 public:
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int sum = 0;
+    int findTargetSumWays(vector<int>& nums, int S) {
         int n = nums.size();
-        for(int  i = 0 ; i < n ; i++){
-            sum += nums[i];
-        }
-        // cout << n ;
-        if(sum < target || (sum + target)%2!=0){
-            return 0;
-        }
-        
-        int s1 = (target + sum)/2;
-        
-        int dp[n+1][s1+1];
-        
-        for(int  i = 0 ; i <= s1 ; i++){
-            dp[0][i] = 0;
-        }
-        
-        for(int i = 0 ; i <= n ; i++){
-            dp[i][0] = 1;
-        }
-        
-        for(int i = 1 ; i <= n ; i++){
-            for(int j = 0 ; j <= s1 ; j++){
-                if(j < nums[i-1]){
-                    dp[i][j] = dp[i-1][j];
-                }
-                else {
-                    dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i-1]];
-                }
+        vector<unordered_map<int,int>> dp(n+1);
+        dp[0][0]=1;
+        for(int i=0;i<n;i++)
+            for(auto &p:dp[i]) {
+                dp[i+1][p.first+nums[i]] += p.second; 
+                dp[i+1][p.first-nums[i]] += p.second;
             }
-        }
-        
-        return dp[n][s1];
-        
-    }
+        return dp[n][S];
+    }
 };
