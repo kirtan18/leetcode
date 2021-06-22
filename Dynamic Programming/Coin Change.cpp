e47653ddd350e860a95a0975a8eef5c67df54300
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-06-22 22:10:58 +0530

    Update Coin Change.cpp

--- Dynamic Programming/Coin Change.cpp
+++ Dynamic Programming/Coin Change.cpp
@@ -1,7 +1,5 @@
 // https://practice.geeksforgeeks.org/problems/coin-change2448/1#
 
-
-
 //  This method in we use knapsack unbounded method bcs in this question in value use over 1 times so 
 
  long long int count( int S[], int m, int n )
@@ -28,3 +26,24 @@
       
       return dp[m][n];
     }
+   
+
+/////////////////////////////////////////////   Recursive Approch /////////////////////////////////////////
+
+
+ int count( int S[], int m, int n )
+{
+  
+    if (n == 0)
+        return 1;
+     
+    if (n < 0)
+        return 0;
+
+    if (m <=0 && n >= 1)
+        return 0;
+
+    return count( S, m - 1, n ) + count( S, m, n-S[m-1] );
+}
+
+
