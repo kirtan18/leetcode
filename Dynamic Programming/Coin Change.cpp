96d5cf25328fcd4d6f101d347f5c3c95ec03f740
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-06-22 21:57:42 +0530

    Coin Change[Medium]

---
+++ Dynamic Programming/Coin Change.cpp
@@ -0,0 +1,30 @@
+// https://practice.geeksforgeeks.org/problems/coin-change2448/1#
+
+
+
+//  This method in we use knapsack unbounded method bcs in this question in value use over 1 times so 
+
+ long long int count( int S[], int m, int n )
+    {
+      long long int dp[m+1][n+1];
+      
+      for(int i = 1 ; i <= n ; i++){
+          dp[0][i] = 0;
+      }
+      for(int j = 1 ; j <= m ; j++){
+          dp[j][0] = 1;
+      }
+      
+      for(int i = 1 ; i <= m ; i++){
+          for(int j = 1 ; j <= n ; j++){
+              if(S[i-1] > j){
+                  dp[i][j] = dp[i-1][j];
+              }
+              else{
+                  dp[i][j] = dp[i][j - S[i-1]] + dp[i-1][j];
+              }
+          }
+      }
+      
+      return dp[m][n];
+    }
