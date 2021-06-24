d190bbf0a15a000a7e468a8c43502faee9a84635
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-06-24 17:34:23 +0530

    Binary Tree Maximum Path Sum[Hard]

---
+++ Tree/Binary Tree Maximum Path Sum.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/binary-tree-maximum-path-sum/
+
+
+
+
+
+class Solution {
+    
+    int MaximumSum(TreeNode * root, int &res){
+        if(!root)return 0;
+        int l = MaximumSum(root->left,res);
+        int r = MaximumSum(root->right,res);
+        // cout << l << " " << r <<" " <<"\n";
+        int temp = max(max(l,r)+root->val , root->val);
+        int ans = max(temp,l+r+root->val);
+        
+        res = max(res,ans);
+        // cout << temp<<"*" ;
+        return temp;
+    }
+    
+public:
+    int maxPathSum(TreeNode* root) {
+        int res = INT_MIN;
+        MaximumSum(root,res);
+        return res;
+    }
+};
