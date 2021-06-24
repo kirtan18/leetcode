0973e680292be939d4c9bd8462be461a84fe532b
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-06-24 10:44:41 +0530

    Find whether it is possible to finish all tasks or not from given dependencies[Medium]

---
+++ Graphs/Find Whether it is possible to finish all tasks or not from given dependencies.cpp
@@ -0,0 +1,5 @@
+// https://www.geeksforgeeks.org/find-whether-it-is-possible-to-finish-all-tasks-or-not-from-given-dependencies/
+
+
+
+This question in Detect cycle if cycle then return fasle otherwise return true
