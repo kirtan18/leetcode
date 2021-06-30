aa589b19afe03d35db96c26922d8a9c1aeb2becd
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-06-30 22:08:56 +0530

    Minimum Deletions in a string to make it a palindrome[Medium]

---
+++ Dynamic Programming/Minimum Deletions in a string to make it a palindrome.cpp
@@ -0,0 +1,36 @@
+// https://practice.geeksforgeeks.org/problems/minimum-deletitions1648/1#
+
+
+  class Solution{
+    int helper(string s,string x){
+         int m = s.size();
+         int n = x.size();
+
+         int dp[m+1][n+1];
+     
+         for(int i = 0 ; i <= m ; i++){
+             for(int j = 0 ; j <= n ; j++){
+                 if(i == 0 || j == 0)dp[i][j] = 0;
+            
+                 else if(s[i-1] == x[j-1]){
+                     dp[i][j] = 1 + dp[i-1][j-1];
+                  }
+
+                 else{
+                     dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+                   }
+
+             }
+          }
+
+     return dp[m][n];
+    }
+  public:
+    int minimumNumberOfDeletions(string s) { 
+           string t = s;
+          reverse(s.begin(), s.end());
+          int ans =  helper(t,s);
+          
+          return s.size() - ans;
+    } 
+};
