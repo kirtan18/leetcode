e7bd7f9eb4084d05ca5051327c0a3d4e0f6e31ee
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-07-06 19:03:54 +0530

    Peak Index in a Mountain Array[Easy]

---
+++ Binary Search/Peak Index in a Mountain Array.cpp
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/peak-index-in-a-mountain-array/
+
+
+//////////////////////////////////////////  Brute  || T.C = O(N) || S.C = O(1) ///////////////////////////////
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        
+        for(int i = 0 ; i < arr.size() -1 ; i++){
+            if(arr[i] > arr[i+1]){
+                return i;
+            }
+        }
+        return 0;
+    }
+};
+
+///////////////////////////////////////////// Optimal  || T.C = O(logn) || S.C = O(1) //////////////////////////////////////////
+
+
+ class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        
+        int l = 0;
+        int r = arr.size()-1;
+        int mid;
+        
+        while( l <= r){
+            mid = l + (r-l) / 2;
+            
+            if(arr[mid] < arr[mid+1]){
+                l = mid+1;
+            }
+            else{
+                r = mid-1;
+            }            
+        }       
+        return l;      
+    }
+};
