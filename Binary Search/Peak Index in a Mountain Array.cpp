b8f13c261ab908696de0d15610d6c70ae0a884c9
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-07-06 19:06:32 +0530

    Update Peak Index in a Mountain Array.cpp

--- Binary Search/Peak Index in a Mountain Array.cpp
+++ Binary Search/Peak Index in a Mountain Array.cpp
@@ -40,3 +40,19 @@ public:
         return l;      
     }
 };
+
+
+/////////////////////////////////////////// One line  //////////////////////////////////
+
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        
+      return max_element(arr.begin(),arr.end())-arr.begin();
+        
+    }
+};
+
+
+
