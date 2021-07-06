1c5b985c1d506733ef344d39a482eb7af709dadf
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-07-06 18:21:33 +0530

    Update Sqrt(x).cpp

--- Binary Search/Sqrt(x).cpp
+++ Binary Search/Sqrt(x).cpp
@@ -7,11 +7,9 @@
 public:
     int mySqrt(int x) {
         
-        double root = 1;
-         int i = 0;
-    
-    
-    
+     double root = 1;
+     int i = 0;
+     
     while (1)
     {
         i = i + 1;
@@ -34,3 +32,36 @@ public:
         return r;
     }
 };
+
+
+
+////////////////////////////////  Optimal using Binary Search  T.C = O(logn) || S.C = O(1)  ///////////////////////////////////
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l = 1;
+        int h = x;
+        int m;
+        
+        while(l <= h){
+            m = l + (h - l) / 2;
+            if( m == x/m){ //for avoid exceed m*m = x so 
+                return m;
+            }
+            else if(m > x/m){
+                h = m-1;
+            }
+            else{
+                l = m+1;
+            }
+        }
+        return l-1;
+    }
+};
+
+
+
+
+
+
