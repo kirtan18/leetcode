6bc3f1a1fada92230fa58d47c1d3cebe7da2096d
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-07-20 22:10:27 +0530

    Update Friends Pairing Problem .cpp

--- Dynamic Programming/Friends Pairing Problem .cpp
+++ Dynamic Programming/Friends Pairing Problem .cpp
@@ -2,23 +2,25 @@ https://practice.geeksforgeeks.org/problems/friends-pairing-problem5425/1#
 
 ///////////////////////////////////////////////////  T.C = O(N) || S.C = O(N)  ///////////////////////////////////////////
 
-class Solution
-{
 public:
+    int mod = 1000000007;
+    int solve(int n , long long int dp[]){
+        if(n <= 2)return n;
+        if(dp[n] != -1)return dp[n];
+        
+        long long int a = (((n-1)%mod) * solve(n-2,dp)%mod)%mod ;
+        long long int b =  solve(n-1,dp)%mod;
+        
+        return dp[n] = a+b;
+    }
+   
     int countFriendsPairings(int n) 
     { 
-        int dp[n + 1];
- 
-    for (int i = 0; i <= n; i++) {
-        if (i <= 2)
-            dp[i] = i;
-        else
-            dp[i] = dp[i - 1] + (i - 1) * dp[i - 2];
+       long long int dp[n+1];
+       memset(dp,-1,sizeof dp);
+       return solve(n , dp) % mod;
     }
- 
-    return dp[n];
-    }
-};   
+};     
 
 /////////////////////////////////////////////////// T.C = O(N) || S.C = O(N) //////////////////////////////////////////////////
 
@@ -54,6 +56,25 @@ public:
         return c % mod;
     }
 }; 
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+public:
+int dp[1000009];
+    int countFriendsPairings(int n) 
+    { 
+       dp[n+1];
+       dp[0] = 0;
+       dp[1] = 1;
+       dp[2] = 2;
+       int mod = 1000000009;
+       for(long long int i = 3 ; i <= n ; i++){
+           dp[i] = ((dp[i-1])%mod + (((i-1) %mod) * (dp[i-2] % mod))) % mod;
+       }
+       return dp[n];
+    }
+}; 
+
 
 
 
