8dafc5e3c1edc9c8e40903e2ae5ab932052967b4
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-09-06 22:21:43 +0530

    Slowest Key[Easy]

---
+++ String/Slowest Key.cpp
@@ -0,0 +1,25 @@
+https://leetcode.com/problems/slowest-key/
+
+
+class Solution {
+public:
+    char slowestKey(vector<int>& releaseTimes, string keysPressed) {
+        int time = releaseTimes[0];
+        char key = keysPressed[0];
+        int new_time = 0;
+        
+        for(int i = 1 ; i < releaseTimes.size() ; i++){
+            
+            new_time = releaseTimes[i] - releaseTimes[i-1];
+            
+            if(new_time == time){
+                key = keysPressed[i] > key ? keysPressed[i] : key;
+            }
+            else if(new_time > time){
+                time = new_time;
+                key = keysPressed[i];
+            }
+        }
+        return key;
+    }
+};
