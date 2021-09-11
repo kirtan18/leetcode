dc58d2f72e7b06ce45b456a9ad08a9e3d12f5429
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-09-11 22:56:26 +0530

    Update Letter Combinations of a Phone Number.cpp

--- String/Letter Combinations of a Phone Number.cpp
+++ String/Letter Combinations of a Phone Number.cpp
@@ -1,5 +1,35 @@
 https://leetcode.com/problems/letter-combinations-of-a-phone-number/
 
+class Solution {
+public:
+    
+    void dfs(string digits ,vector<string>& ans,string nums[],int i, string s){
+        
+        if(i == digits.length()){
+            ans.push_back(s);
+            return;
+        }
+        string val = nums[digits[i] - '0'];
+        
+        for(int j=0;j<val.length(); j++)
+            dfs(digits, ans, nums, i+1, s+val[j]);
+        
+    }
+    
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        
+        if(digits.length() == 0) return ans;
+        
+        string nums[10] ={
+          "","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"  
+        };
+        dfs(digits,ans,nums,0,"");
+        return ans;
+    }
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Solution {
     
