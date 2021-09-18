53bf6f8268763ae20a104b5614e9a2b1df0473e0
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-09-18 10:34:05 +0530

    Update Binomial Coefficient.cpp

--- Dynamic Programming/Binomial Coefficient.cpp
+++ Dynamic Programming/Binomial Coefficient.cpp
@@ -36,3 +36,32 @@ public:
         return dp[r];
     }
 };
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+class Solution{
+public:
+    int nCr(int n, int k){
+        int mod = 1000000007;
+        long long int C[n + 1][k + 1]; 
+        int i, j; 
+        if(n < k) return 0;
+      
+        for (i = 0; i <= n; i++) 
+        { 
+            for (j = 0; j <= min(i, k); j++) 
+            { 
+
+                if (j == 0 || j == i) 
+                    C[i][j] = 1; 
+     
+                else
+                    C[i][j] = (C[i - 1][j - 1] +  C[i - 1][j]) % mod; 
+            } 
+        } 
+      
+        return C[n][k]; 
+
+    }
+};
