bb49c98b9e1dd57ada0d8f6fe2a153405054db28
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-10-22 20:59:12 +0530

    Update Longest Increasing Subsequence .cpp

--- Dynamic Programming/Longest Increasing Subsequence .cpp
+++ Dynamic Programming/Longest Increasing Subsequence .cpp
@@ -1,56 +1,53 @@
 https://practice.geeksforgeeks.org/problems/longest-increasing-subsequence-1587115620/1#
 
-////////////////////////////////////////////////////////  Brute || T.c = O(N^2) TLE || S.C = O(N) ////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////  Brute || T.c = O(N^2) || S.C = O(N) ////////////////////////////////////////////////////////
 
-int longestSubsequence(int n, int a[])
-    {
-       int dp[n];
-       
-       for(int i = 0 ; i < n ; i++)dp[i] = 1;
-       
-       for(int i = 1 ; i < n ; i++){
-           for(int j = 0 ; j < i ; j++){
-               if(a[j] < a[i]){
-                   dp[i] = max(dp[i], dp[j] + 1);
-               }
-           }
-       }
-       
-       int maxi = INT_MIN;
-       for(int i = 0 ; i < n ;i++){
-          maxi = max(maxi,dp[i]);
-       }
-       return maxi;
-    }
+class Solution {
+public:
+	int lengthOfLIS(vector<int>& nums) {
+
+		int n = nums.size();
+
+		vector<int>LIS(n+1, 1);
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < i; j++) {
+				if (nums[i] > nums[j])
+					LIS[i] = max(LIS[i], 1 + LIS[j]);
+			}
+		}
+
+		int ans = 0;
+		for (int i = 0; i < n; i++) {
+			ans = max(ans, LIS[i]);
+		}
+
+		return ans;
+	}
+};
 
 /////////////////////////////////////////////////////////// Optimal || T.C = O(n ^ logn) || S.C = O(N)  ///////////////////////////////////////////////////////
 
 
-class Solution
-{
-    public:
-    //Function to find length of longest increasing subsequence.
-    int longestSubsequence(int n, int a[])
-    {
-       int dp[n+1];
-       for(int i = 1 ; i <= n ; i++) dp[i] = INT_MAX;
-       dp[0] = INT_MIN;
-       
-       for(int i = 0 ; i < n ; i++){
-           int idx = upper_bound(dp , dp+n+1 , a[i]) - dp;
-           if( dp[idx-1] < a[i] && dp[idx] > a[i] ){
-               dp[idx] = a[i];
-           }
-       }
-       
-       int ma = 0;
-       
-       for(int i = n ; i >= 0 ; i--){
-           if(dp[i] != INT_MAX){
-               ma = i;
-               break;
-           }
-       }
-       return ma;
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<int>seq;
+        seq.push_back(nums[0]);
+        
+        for(int i=1;i<n;i++){
+            if(seq.back() < nums[i]){
+                seq.push_back(nums[i]);
+            }
+            else{
+                int ind = lower_bound(seq.begin(),seq.end(),nums[i]) - seq.begin();
+                cout << ind << " ";
+                seq[ind] = nums[i];
+            }
+        }
+        
+        return seq.size();
     }
 };
