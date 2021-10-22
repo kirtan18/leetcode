0146b6547491bdb4782968b3ea8a9b6ce6fd54fb
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-10-22 14:06:04 +0530

    Tiling With 2 * 1 Tiles [Medium]

---
+++ Dynamic Programming/Tiling With 2 * 1 Tiles.cpp
@@ -0,0 +1,23 @@
+https://www.pepcoding.com/resources/online-java-foundation/dynamic-programming-and-greedy/tiling1-official/ojquestion
+
+
+#include <bits/stdc++.h>
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+     int n;
+     cin >> n;
+
+     int dp[n];
+     dp[0] = 1;
+     dp[1] = 2;
+
+     for(int i=2;i<n;i++){
+        dp[i] = dp[i-1] + dp[i-2];
+     }
+
+     cout << dp[n-1] << "\n";
+     return 0;
+}
