5f4e72693c5d5bc0d3f493f38411964c9fe07efe
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-11-17 21:39:56 +0530

    Unique Paths [Medium] DP

---
+++ DFS/Unique Paths.cpp
@@ -0,0 +1,46 @@
+https://leetcode.com/problems/unique-paths/
+
+///////////////////////// T.C = O(MN) /////////////////////////////////////
+
+class Solution {
+    
+    int solve(int i,int j,int m,int n,vector<vector<int>>&dp){
+        if(i>=m || j>=n){
+            return 0;
+        }
+        if(i == m-1 && j == n-1){
+            return 1;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        return dp[i][j] =  solve(i+1,j,m,n,dp)+solve(i,j+1,m,n,dp);
+        
+    }
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return solve(0,0,m,n,dp);
+    }
+};
+
+
+//////////////////////// T.C = O(MN) //////////////////////////////////
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int dp[m][n];
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i == 0 || j == 0 ){
+                    dp[i][j] = 1;
+                }else{
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
