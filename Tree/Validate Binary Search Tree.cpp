1f76cb0e0b815231e85b9c5c5d19e3314ef71a9e
kirtan prajapati <68524627+kirtan18@users.noreply.github.com>
2021-12-10 19:08:06 +0530

    Update Validate Binary Search Tree.cpp

--- Tree/Validate Binary Search Tree.cpp
+++ Tree/Validate Binary Search Tree.cpp
@@ -1,6 +1,6 @@
-  https://leetcode.com/problems/validate-binary-search-tree/
+  https://leetcode.com/problems/validate-binary-search-tree/ 
   
-    ///////////////////////////////////////////  Best Method  /////////////////////////////////////////////
+///////////////////////////////////////////  Best Method  /////////////////////////////////////////////
     
     
     class Solution {
@@ -27,6 +27,26 @@ private:
     vector<int> tree;
 };
 
+////////////////////////////////////////////// Using BFS ///////////////////////////////////////////////////
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        queue<tuple<TreeNode*,long,long>>q;
+        q.push(root,LONG_MIN,LONG_MAX);
+        
+        while(!q.empty()){
+             TreeNode*node = get<0>(q.front());
+             long lower = get<1>(q.front());
+             long upper = get<2>(q,front()); 
+             q.pop();
+            
+             if(node->val <= lower || node->val >= upper)return false;
+             if(node->left != NULL) q.emplace(node->left,lower,node->val);
+             if(node->right != NULL) q.emplace(node->right,node->val,upper);
+        }
+        return true;
+    }
+};
 
 ///////////////////////////////////////////////////  2nd  method for reduse S.C ///////////////////////////////////////////
 
